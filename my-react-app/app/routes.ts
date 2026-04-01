b54515bec396cdb0d786a3f77197ba91bc80dc33
Sbahdi <aitsalaheddine2009@gmail.com>
2026-04-01 17:09:57 +0100

    -_-

--- my-react-app/app/routes.ts
+++ my-react-app/app/routes.ts
@@ -3,6 +3,6 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
   index("routes/home/home.tsx"),
   route("components", "./routes/infos/infos.tsx"),
-  route("profile", "./routes/profile/Profile.tsx", { id: "editProfile" }),
+  route("profile", "./routes/profile/Profile.tsx"),
   route("profileSettings", "./routes/editProfile/editProfile.tsx"),
 ] satisfies RouteConfig;
